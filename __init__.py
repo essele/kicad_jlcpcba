7274887243afa69ea38e79482bc4371619e34b8f
Lee Essen <lee.essen@nowonline.co.uk>
2020-02-08 16:35:52 +0000

    updated to support main

--- __init__.py
+++ __init__.py
@@ -1,2 +1,9 @@
-from .jlcpcba_action import JlcpcbaPluginAction
-JlcpcbaPluginAction().register()
+try:
+    from .jlcpcba_action import JlcpcbaPluginAction
+    JlcpcbaPluginAction().register()
+except Exception as e:
+    import os
+    plugin_dir = os.path.dirname(os.path.realpath(__file__))
+    log_file = os.path.join(plugin_dir, 'jlcpcb_reg.log')
+    with open(log_file, 'w') as f:
+        f.write(repr(e))
--- jlcpcba_action.py
+++ jlcpcba_action.py
@@ -1,7 +1,10 @@
 import pcbnew
 import wx
+import sys
 import os
 import re
+
+from .jlcpcba_main import *
 
 class JlcpcbaPluginAction(pcbnew.ActionPlugin):
     def defaults(self):
@@ -13,6 +16,17 @@ class JlcpcbaPluginAction(pcbnew.ActionPlugin):
 
     def Run(self):
         print("Testing")
-        wx.MessageDialog(None, "Hello World").ShowModal()
+        try:
+            create()
+            wx.MessageDialog(None, "All Done").ShowModal()
+        except Exception as e:
+            import os
+            plugin_dir = os.path.dirname(os.path.realpath(__file__))
+            log_file = os.path.join(plugin_dir, 'jlcpcba_run.log')
+            with open(log_file, 'w') as f:
+                f.write(repr(e))
+            wx.MessageDialog(None, "Failed, check logs").ShowModal()
+
+
 
 
